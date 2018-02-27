4f692eff165bd9f0028814241d9df117df84d1ea
ChandlerXAAP <ChandlerXAAP@yandex.ru>
2018-02-27 21:43:49 +0300

    1.1. Создание Hello World [#141]

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+public class Calculate
+// Задаем класс Calculate
+{
+	public static void main(String[] args)
+	//задаем метод main 
+	
+	{
+		System.out.println("Hello world!");
+		//Тело метода, который содержит объект System.out, метод println(отображает текст в виде строки(тип string))  и параметр(Hello world)
+	}
+}
