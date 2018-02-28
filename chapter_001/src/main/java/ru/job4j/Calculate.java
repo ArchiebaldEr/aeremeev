7fcdb49926dc655db3982b57da009f7de8cddc63
ChandlerXAAP <ChandlerXAAP@yandex.ru>
2018-02-28 14:17:59 +0300

    1.2. Подключить сборщик Maven[#4086]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,8 @@
 package ru.job4j;
-public class Calculate
+public class Calculate {
 // Задаем класс Calculate
-{
-	public static void main(String[] args)
+	public static void main(String[] args) {
 	//задаем метод main 
-	
-	{
 		System.out.println("Hello world!");
 		//Тело метода, который содержит объект System.out, метод println(отображает текст в виде строки(тип string))  и параметр(Hello world)
 	}
