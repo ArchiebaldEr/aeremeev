50915e44797bc1001a0043f1fa39fe2aae20cf18
ChandlerXAAP <ChandlerXAAP@yandex.ru>
2018-03-09 16:52:59 +0300

    3.3. Идеальный вес. [#41584]

--- chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
@@ -6,18 +6,19 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FitTest {
-
+    final double manHeight = 180;
+    final double womanHeight = 170;
     @Test
     public void manWeight() {
         Fit fit = new Fit();
-        double weight = fit.manWeight(180);
+        double weight = fit.manWeight(manHeight);
         assertThat(weight, is(92D));
     }
 
     @Test
     public void womanWeight() {
         Fit fit = new Fit();
-        double weight = fit.womanWeight(170);
+        double weight = fit.womanWeight(womanHeight);
         assertThat(weight, is(69D));
     }
 }
